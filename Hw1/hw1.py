1246ef3222e26b532b4397112420190e91fe5512
pinman <pinmanhuang@gmail.com>
2020-10-13 09:39:34 +0800

    augmented reality done

--- Hw1/hw1.py
+++ Hw1/hw1.py
@@ -27,38 +27,47 @@ class PyMainWindow(QMainWindow, Ui_MainWindow):
         self.show()
 
         # === Q1 ===
-        self.opencv = OpenCv()
-        self.opencv.Q1()
         self.q1_3_img_idx = 0
 
     """
     ref: https://docs.opencv.org/3.4/dc/dbb/tutorial_py_calibration.html
     """ 
     def find_corner(self):
-        print('find corner')
-        self.opencv.Q1_1()
+        print('Find corner')
+        opencv = OpenCv()
+        opencv.Q1()
+        opencv.Q1_1()
 
     def find_intrinsic(self):
-        print('find intrinsic')
-        self.opencv.Q1_2()
+        print('Find intrinsic')
+        opencv = OpenCv()
+        opencv.Q1()
+        opencv.Q1_2()
 
     def find_distortion(self):
-        print('find distortion')
-        self.opencv.Q1_4()
+        print('Find distortion')
+        opencv = OpenCv()
+        opencv.Q1()
+        opencv.Q1_4()
     
     def find_extrinsic(self):
-        print('find extrinsic')
-        self.opencv.Q1_3(self.q1_3_img_idx)
+        print('Find extrinsic')
+        opencv = OpenCv()
+        opencv.Q1()
+        opencv.Q1_3(self.q1_3_img_idx)
 
     def select_image(self, text):
-        print('select image: {}'.format(str(int(text)+1)))
         self.q1_3_img_idx = int(text)
 
     def augmented_reality(self):
-        print('augmented reality')
+        print('Augmented reality')
+        opencv = OpenCv()
+        opencv.Q2()
 
     def stereo_disparity(self):
-        print('stereo disparity')
+        print('Stereo disparity')
+        opencv = OpenCv()
+        opencv.Q3()
         
 
 class OpenCv(object):
@@ -73,31 +82,25 @@ class OpenCv(object):
         # ==========
 
     def Q1(self):
-        # termination criteria
-        criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
-
+        print('calculate...')
         # Prepare object points, like (0, 0, 0), (1, 0, 0), ..., (7, 10, 0)
         objp = np.zeros((8*11, 3), np.float32)
         objp[:, :2] = np.mgrid[0:11, 0:8].T.reshape(-1, 2)
         
         for f_idx in range(1, 16):
-            print(f_idx)
             # Read image and converty to gray image
             img = cv2.imread('./Q1_Image/'+str(f_idx)+'.bmp')
             gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
             self.gray_img[f_idx-1] = gray
-            
             # Find the chess board corners
             ret, corners = cv2.findChessboardCorners(gray, (11, 8), None)
-            
+
             # If found, add object points, image points
             if ret == True:
                 self.objpoints.append(objp)
                 self.imgpoints.append(corners)
-
                 # Draw and display the corner
                 cv2.drawChessboardCorners(img, (11, 8), corners, ret)
-
                 # save corner images
                 self.corner_img[f_idx-1] = img
             
@@ -105,14 +108,13 @@ class OpenCv(object):
     def Q1_1(self):
         print('Q1_1')
         for f_idx in range(1, 16):
-            print(f_idx)
             cv2.namedWindow('1.1 Find Corners: '+str(f_idx)+'.bmp', cv2.WINDOW_NORMAL)
             cv2.imshow('1.1 Find Corners: '+str(f_idx)+'.bmp', self.corner_img[f_idx-1])
         cv2.waitKey(0)
         cv2.destroyAllWindows()
     
     def Q1_2(self):
-        print('Q1_2: 15.bmp')
+        print('Q1_2')
         ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(self.objpoints, self.imgpoints, self.gray_img[14].shape[::-1], None, None)
         print(mtx)
 
@@ -125,9 +127,62 @@ class OpenCv(object):
         print(extrinsic_matrix)
 
     def Q1_4(self):
+        print('Q1_4')
         ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(self.objpoints, self.imgpoints, self.gray_img[0].shape[::-1], None, None)
         print(dist)
-        
+
+    def Q2(self):
+        print('Q2')
+        # Prepare object points, like (0, 0, 0), (1, 0, 0), ..., (7, 10, 0)
+        objp = np.zeros((8*11, 3), np.float32)
+        objp[:, :2] = np.mgrid[0:11, 0:8].T.reshape(-1, 2)
+        objpoints = []                             # 3d point in real world space
+        imgpoints = []                             # 2d points in image plane
+
+        # Calculate camera calibration coefficients
+        for f_idx in range(1, 6):
+            # Read image and converty to gray image
+            img = cv2.imread('./Q2_Image/'+str(f_idx)+'.bmp')
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            # Find the chess board corners
+            ret, corners = cv2.findChessboardCorners(gray, (11, 8), None)
+
+            # If found, add object points, image points
+            if ret == True:
+                objpoints.append(objp)
+                imgpoints.append(corners)
+
+        ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+
+        cv2.namedWindow('Augmented Reality', cv2.WINDOW_NORMAL)
+        # Draw a tetrahedron on chessboard
+        for f_idx in range(1, 6):
+            # Read image and converty to gray image
+            img = cv2.imread('./Q2_Image/'+str(f_idx)+'.bmp')
+            red = [0, 0, 255]   # BGR
+            # tetrahefron cooridinates
+            tetrahefron_coor = np.array([(3, 3, -3),
+                                         (3, 5, 0),
+                                         (5, 1, 0),
+                                         (1, 1, 0)], dtype=np.float32)
+            # project 3D points to image plane
+            imgpts, jac = cv2.projectPoints(tetrahefron_coor, rvecs[f_idx-1], tvecs[f_idx-1], mtx, dist)
+            imgpts = np.int32(imgpts).reshape(-1, 2)
+            # draw the tetrahefron 
+            cv2.line(img, tuple(imgpts[0]), tuple(imgpts[1]), red, 10)
+            cv2.line(img, tuple(imgpts[0]), tuple(imgpts[2]), red, 10)
+            cv2.line(img, tuple(imgpts[0]), tuple(imgpts[3]), red, 10)
+            
+            cv2.line(img, tuple(imgpts[1]), tuple(imgpts[2]), red, 10)
+            cv2.line(img, tuple(imgpts[2]), tuple(imgpts[3]), red, 10)
+            cv2.line(img, tuple(imgpts[3]), tuple(imgpts[1]), red, 10)
+
+            cv2.imshow('Augmented Reality', img)
+            cv2.waitKey(500)
+        cv2.destroyAllWindows()
+
+    def Q3(self):
+        print('Q3')        
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
