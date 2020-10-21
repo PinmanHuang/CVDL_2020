384c6bd31482d05b67bb5b20df7f4c258e5883d9
pinman <pinmanhuang@gmail.com>
2020-10-21 14:11:29 +0800

    VGG16

--- Hw1/hw1.py
+++ Hw1/hw1.py
@@ -9,16 +9,19 @@ import sys
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
+# Q5
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
 # Q5_1
 import pickle
 import random
 # Q5_3
 # from torchvision import models
 from torchsummary import summary
-# Q5
-import torch
-import torch.nn as nn
-import torch.optim as optim
+
+
 
 class PyMainWindow(QMainWindow, Ui_MainWindow):
     
@@ -99,28 +102,28 @@ class PyMainWindow(QMainWindow, Ui_MainWindow):
 
     def show_train_images(self):
         print('Show train images')
-        opencv = OpenCv()
-        opencv.Q5_1()
+        model = Model()
+        model.Q5_1()
 
     def show_hyperparameters(self):
         print('Show hyperparameters')
-        opencv = OpenCv()
-        opencv.Q5_2()
+        model = Model()
+        model.Q5_2()
     
     def show_model_strucuture(self):
         print('Show model strucuture')
-        opencv = OpenCv()
-        opencv.Q5_3()
+        model = Model()
+        model.Q5_3()
     
     def show_accuracy(self):
         print('Show accuracy')
-        opencv = OpenCv()
-        opencv.Q5_4()
+        model = Model()
+        model.Q5_4()
 
     def test(self):
         print('Test')
-        opencv = OpenCv()
-        opencv.Q5_5()
+        model = Model()
+        model.Q5_5()
     
 class VGG16(nn.Module):
     def __init__(self, num_classes=10):
@@ -412,6 +415,14 @@ class OpenCv(object):
         cv2.waitKey()
         cv2.destroyAllWindows()
 
+class Model(object):
+    def __init__(self):
+        super(Model, self).__init__()
+        self.batch_size = 256
+        self.learning_rate = 1e-2
+        self.num_epoches = 50
+        self.optimizer = 'SGD'
+        
     '''
     batches.meta: size is 10,
                   0 to 9 class,
@@ -448,7 +459,7 @@ class OpenCv(object):
     
     def Q5_2(self):
         print('Q5_2')
-        print('hyperparameters:\nbatch size: 32\nlearning rate: 0.001\noptimizer: SGD')
+        print('hyperparameters:\nbatch size: '+str(self.batch_size)+'\nlearning rate: '+str(self.learning_rate)+'\noptimizer: '+self.optimizer)
     
     def Q5_3(self):
         print('Q5_3')
@@ -456,10 +467,41 @@ class OpenCv(object):
         model = VGG16().to(device)
         # model = models.vgg16()
         summary(model, (3, 32, 32))
-
     
     def Q5_4(self):
         print('Q5_4')
+        # Load data
+        train_dataset = torchvision.datasets.CIFAR10(root='./', train=True, download=False, transform=torchvision.transforms.ToTensor())
+        train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=self.batch_size, shuffle=True)
+        # Create VGG16 model
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        model = VGG16().to(device)
+        # Define loss and optimizer
+        criterion = nn.CrossEntropyLoss()
+        optimizer = optim.SGD(model.parameters(), lr=self.learning_rate)
+        # Train the model
+        for epoch in range(self.num_epoches):
+            running_loss = 0.0
+            correct_pred = 0
+            for i, data in enumerate(train_loader):
+                img, label = data
+                img = img.to(device)
+                label = label.to(device)
+                # Forwarding
+                y_pred = model(img)
+                _, pred = torch.max(y_pred, 1)          # prediction the index of the maximum value location
+                correct_pred += (pred == label).sum()
+                # Backpropagation
+                loss = criterion(y_pred, label)
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+
+                running_loss += float(loss.item())
+            print('epoch {}/{}\tTrain loss: {:.4f}\tTrain accuracy: {:.2f}%'.format(epoch + 1, self.num_epoches, running_loss / (i + 1), correct_pred.item() / (self.batch_size * (i + 1)) * 100))
+
+
+
 
     def Q5_5(self):
         print('Q5_5')
