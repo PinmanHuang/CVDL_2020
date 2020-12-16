503d05c3c690a2ceb3e460731dfed1b79fe91e02
pinman <pinmanhuang@gmail.com>
2020-12-16 16:11:31 +0800

    2-1 done

--- Hw2/hw2.py
+++ Hw2/hw2.py
@@ -17,10 +17,14 @@ class PyMainWindow(QMainWindow, Ui_MainWindow):
         self.setupUi(self)
         # === push button clicked action === 
         self.pushButton.clicked.connect(self.bg_sub)
+        self.pushButton_2.clicked.connect(self.preprocessing)
 
     def bg_sub(self):
         opencv = OpenCv()
         opencv.Q1_1()
+    def preprocessing(self):
+        opencv = OpenCv()
+        opencv.Q2_1()
 
 class OpenCv(object):
     def __init__(self):
@@ -31,6 +35,7 @@ class OpenCv(object):
         frames = []
         cv2.namedWindow('1.1 Original Video', cv2.WINDOW_NORMAL)
         cv2.namedWindow('1.1 Subtraction Video', cv2.WINDOW_NORMAL)
+        # capture video and get fps
         cap = cv2.VideoCapture('./Q1_Image/bgSub.mp4')
         fps = cap.get(cv2.CAP_PROP_FPS)
         # print("fps: {}".format(fps))
@@ -68,12 +73,50 @@ class OpenCv(object):
                 # print("type: {}, shape: {}, diff_type:{}, diff_shape: {}, mean: {}, std: {}".format(
                 #      type(gray), gray.shape, type(diff), diff.shape, mean.shape, std.shape))
                      
-            # result_img = cv2.from
             cv2.imshow('1.1 Original Video', frame)
             cv2.imshow('1.1 Subtraction Video', result)
             if cv2.waitKey(int(1000/fps)) & 0xFF == ord('q'):
                 break
 
+        cap.release()
+        cv2.destroyAllWindows()
+
+    def Q2_1(self):
+        # 
+        cv2.namedWindow('2.1 Preprocessing Video', cv2.WINDOW_NORMAL)
+        # capture video
+        cap = cv2.VideoCapture('./Q2_Image/opticalFlow.mp4')
+        # get 1st frame of video
+        ret, frame = cap.read()
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        # Set up the detector with parameters
+        params = cv2.SimpleBlobDetector_Params()
+        params.minThreshold = 10
+        params.maxThreshold = 200
+        params.filterByArea = True
+        params.minArea = 35
+        params.filterByCircularity = True
+        params.minCircularity = 0.8
+        params.maxCircularity = 0.9
+        params.filterByConvexity = True
+        params.minConvexity = 0.5
+        params.filterByInertia = True
+        params.minInertiaRatio = 0.5
+        detector = cv2.SimpleBlobDetector_create(params)
+        # Detect blobs
+        keypoints = detector.detect(gray)
+        # Draw rectangle and line
+        for kp in keypoints:
+            x, y = (kp.pt)
+            x = int(x)
+            y = int(y)
+            frame = cv2.rectangle(frame, (x-5, y-5), (x+5, y+5), (0, 0, 255), 1)
+            frame = cv2.line(frame, (x, y-5), (x, y+5), (0, 0, 255), 1)
+            frame = cv2.line(frame, (x-5, y), (x+5, y), (0, 0, 255), 1)
+
+        cv2.imshow('2.1 Preprocessing Video', frame)
+        cv2.waitKey(0)
         cap.release()
         cv2.destroyAllWindows()
 
